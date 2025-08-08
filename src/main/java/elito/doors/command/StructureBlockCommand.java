51d4e762bca929bdb8ee81bc80014b0d074d60d7
ElitoSear <elitogamer100@gmail.com>
2025-08-08 16:29:49 -0600

    Added load option to structure_block command and removed unused command registration.

--- src/main/java/elito/doors/command/StructureBlockCommand.java
+++ src/main/java/elito/doors/command/StructureBlockCommand.java
@@ -63,6 +63,53 @@ public class StructureBlockCommand {
                                                                     return 1;
                                                                 }
                                                         )
+                                        ).then(
+                                                literal("load")
+                                                        .executes(
+                                                                commandContext -> {
+                                                                    ServerCommandSource source = commandContext.getSource();
+
+                                                                    BlockPos blockPos = BlockPosArgumentType.getBlockPos(commandContext, "pos");
+
+                                                                    BlockEntity blockEntity = source.getWorld().getBlockEntity(blockPos);
+
+                                                                    StructureBlockBlockEntity selectedStructureBlock = (StructureBlockBlockEntity) blockEntity;
+
+                                                                    if (selectedStructureBlock == null) {
+                                                                        source.sendFeedback(() -> Text.literal("Structure block not found"), false);
+                                                                        return 0;
+                                                                    }
+
+                                                                    Vec3i size = selectedStructureBlock.getSize();
+
+                                                                    String name = selectedStructureBlock.getTemplateName();
+
+                                                                    StructureBlockMode mode = selectedStructureBlock.getMode();
+
+                                                                    if (getSize(size) < 1) {
+                                                                        source.sendFeedback(() -> Text.literal("Invalid size for structure block"), false);
+                                                                        return 0;
+                                                                    }
+
+                                                                    if (name == null || name.isEmpty()) {
+                                                                        source.sendFeedback(() -> Text.literal("Invalid name for structure block"), false);
+                                                                        return 0;
+                                                                    }
+
+                                                                    if (mode != StructureBlockMode.LOAD) {
+                                                                        source.sendFeedback(() -> Text.literal("Structure block must be in load mode"), false);
+                                                                        return 0;
+                                                                    }
+
+                                                                    if (selectedStructureBlock.loadAndTryPlaceStructure(source.getWorld())) {
+                                                                        source.sendFeedback(() -> Text.literal("Loaded structure block successfully"), false);
+                                                                        return 1;
+                                                                    } else {
+                                                                        source.sendFeedback(() -> Text.literal("Failed to load structure block"), false);
+                                                                        return 0;
+                                                                    }
+                                                                }
+                                                        )
                                         )
                         )
                 ));
